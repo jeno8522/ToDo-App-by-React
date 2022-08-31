e43afabd6dafc569e4287d437863a8e81ffeb36d
jeno8522 <68889383+jeno8522@users.noreply.github.com>
2022-08-31 17:50:12 +0900

    todo app

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#9c88ff",
+};
+
+export { darkTheme };
